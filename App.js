27a6f4c24e890b4331b46b51d06e03e87408da58
Trex275 <73885729+Trex275@users.noreply.github.com>
2021-07-06 16:59:52 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,32 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import 'react-native-gesture-handler'
+import 'react-native-gesture-handler';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/Home';
-import IssLocationScreen from './screens/IssLocation';
-import MeteorScreen from './screens/Meteors';
 
-const Stack = createStackNavigator()
+import HomeScreen from "./screens/Home";
+import IssLocationScreen from "./screens/IssLocation";
+import MeteorScreen from "./screens/Meteors";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        headerShown: false
+      }}>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="ISSLocation" component={IssLocationScreen} />
-        <Stack.Screen name="Meteor" component={MeteorScreen} />
+        <Stack.Screen name="IssLocation" component={IssLocationScreen} />
+        <Stack.Screen name="Meteors" component={MeteorScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
